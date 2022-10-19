1ab405bdf424d021008e647669796f429f4d84fe
EDyugaeva <ed2408@yandex.ru>
2022-10-19 21:51:57 +0300

    One response, start develop

---
+++ src/main/java/com/example/bot/sevice/TelegramBot.java
@@ -0,0 +1,54 @@
+package com.example.bot.sevice;
+
+import com.example.bot.configuration.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig configuration;
+
+    public TelegramBot(BotConfig configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return configuration.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return configuration.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            sendMessage("Привет!!!", chatId);
+        }
+
+    }
+
+    private void sendMessage(String message, long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
